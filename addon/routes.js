0e262a9909bf8f0e9a789d1a9adc7e2c71f59c19
Nóra Szepes <nora.szepes@tenforce.com>
2017-10-10 15:59:38 +0200

    removed unsued routes

--- addon/routes.js
+++ addon/routes.js
@@ -1,14 +1,6 @@
 import buildRoutes from 'ember-engines/routes';
 
 export default buildRoutes(function() {
-  this.route('pipelines', function() {
-    this.route('index');
-    this.route('new');
-    this.route('steps', { path: 'steps/:pipeline_id' }, function() {
-      this.route('index');
-      this.route('new');
-    });
-  });
   this.route('index');
   this.route('new');
   this.route('steps', { path: ':pipeline_id' }, function() {
