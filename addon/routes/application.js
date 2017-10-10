0c0884c2538a675c28bd2dcd6f4829af166d1dc1
Nóra Szepes <nora.szepes@tenforce.com>
2017-10-10 15:03:26 +0200

    removed auto redirect to pipelines.index

--- addon/routes/application.js
+++ addon/routes/application.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 import LoadingSliderMixin from 'ember-cli-loading-slider/mixins/loading-slider';
 
 const ApplicationRoute = Ember.Route.extend(LoadingSliderMixin, {
-    beforeModel() {
-        this.transitionTo('pipelines.index');
-    }
 });
 
 export default ApplicationRoute;
