cc5b350533b3fc7cc95dd45e649e15d6a5f8122f
Nóra Szepes <nora.szepes@tenforce.com>
2017-10-10 16:00:18 +0200

    removed index from the routes
    then /index won't appear in the url

--- addon/routes.js
+++ addon/routes.js
@@ -1,10 +1,8 @@
 import buildRoutes from 'ember-engines/routes';
 
 export default buildRoutes(function() {
-  this.route('index');
   this.route('new');
   this.route('steps', { path: ':pipeline_id' }, function() {
-    this.route('index');
     this.route('new');
   });
 });
