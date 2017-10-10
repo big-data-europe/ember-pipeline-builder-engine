92d7cb207f0d777f4d47892cbc0e572294a9d1eb
Nóra Szepes <nora.szepes@tenforce.com>
2017-10-10 15:05:38 +0200

    added relatedWorkflow queryparam

--- addon/controllers/pipelines/steps.js
+++ addon/controllers/pipelines/steps.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  classNames: ['pipeline-steps']
+  classNames: ['pipeline-steps'],
+  queryParams: ['relatedWorkflow'],
+  relatedWorkflow: ""
 });
--- addon/routes/pipelines/steps.js
+++ addon/routes/pipelines/steps.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 const PipelinesStepsRoute = Ember.Route.extend({
+  queryParams: {
+    relatedWorkflow: {
+      refreshModel: true
+    }
+  },
   store: Ember.inject.service('store'),
   model(params) {
     return this.get('store').findRecord('pipeline', params.pipeline_id);
